d29ed93337d82735274789e76dec739aa9d97e7a
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2017-12-07 13:52:19 -0300

    Add models in admin file

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(Generator)
+admin.site.register(Phrase)
+admin.site.register(PhraseAudio)
+admin.site.register(RelationPhrase)
+admin.site.register(Producer)
+admin.site.register(Production)
+admin.site.register(Season)
+admin.site.register(Episode)
