0dcf12bcf7f6d189f6eba2f6467da07bd73fa25b
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-03-17 07:10:45 -0300

    Add user with django user and not userapp

--- language/models/generator.py
+++ language/models/generator.py
@@ -1,8 +1,9 @@
 
+from django.contrib.auth.models import User
 from django.db import models
 
 class Generator(models.Model):
-    user = models.ForeignKey('UserApp', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     repeat = models.SmallIntegerField()
     translate_repeat = models.SmallIntegerField()
