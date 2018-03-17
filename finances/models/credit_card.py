48aaa3d5c3721d4d3f76a1578ce5ac34f8192872
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-03-17 13:55:58 -0300

    Update credit card model
    - Add user
    - Change bill_date to bill_day
    - Add Currency
    - Add __str__ method
    - Remove choices from file
    - Add choices from choices file

--- finances/models/credit_card.py
+++ finances/models/credit_card.py
@@ -1,34 +1,63 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
-TYPE_CREDIT_CARD = (
-		(1, 'Visa'),
-		(2, 'Mastercard'),
-		(3, 'American Express'),
-		(4, 'Another'),
-	)
+from .choices import (
+	TYPE_CREDIT_CARD_CHOICES,
+	CURRENCY_CHOICES
+)
+
 
 class CreditCard(models.Model):
-	name = models.CharField(max_length=50)
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE
+	)
+	name = models.CharField(
+		max_length=50
+	)
 	type = models.PositiveSmallIntegerField(
-			choices=TYPE_CREDIT_CARD
-		)
+		choices=TYPE_CREDIT_CARD_CHOICES
+	)
 	limit = models.PositiveIntegerField()
-	bill_date = models.DateField()
+	bill_day = models.PositiveSmallIntegerField(
+		validators=[
+			MaxValueValidator(31), 
+			MinValueValidator(1)
+		]
+	)
+	currency = models.CharField(
+		max_length=3, 
+		default='USD',
+		choices=CURRENCY_CHOICES
+	)
 
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
-	def __unicode__(self):
+	def __str__(self):
 		return self.name
 
 class CreditCardBill(models.Model):
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE
+	)
 	credit_card = models.ForeignKey(
 			'CreditCard',
 			on_delete=models.CASCADE
 		)
-	file = models.FileField(upload_to='bills/')
+	file = models.FileField(
+		upload_to='bills/'
+	)
 	total = models.PositiveIntegerField()
 	paid = models.DateTimeField()
 
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return '{} {}'.format(
+			self.total,
+			self.paid
+		)
