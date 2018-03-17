fef0a40e99d80d1a1ce762bbb775f1179fb4c7fe
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-03-17 13:49:50 -0300

    Add better imports

--- finances/models/__init__.py
+++ finances/models/__init__.py
@@ -1,5 +1,19 @@
-from .credit_card import *
-from .expense import *
-from .income import *
-from .installment import *
-from .type_expense import *
+from .choices import (
+		TYPE_CREDIT_CARD_CHOICES,
+		METHOD_TYPE_CHOICES
+	)
+from .credit_card import CreditCard, CreditCardBill
+from .expense import Expense
+from .income import Income
+from .installment import Installment
+from .type_expense import CategoryTypeExpense, TypeExpense
+
+__all__ = [
+	'CreditCard',
+	'CreditCardBill',
+	'Expense',
+	'Income',
+	'Installment',
+	'CategoryTypeExpense',
+	'TypeExpense'
+]
