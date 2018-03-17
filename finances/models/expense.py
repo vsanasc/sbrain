80cc2f3a01caa2d6bf991dd03f58e4a8d2800c56
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-03-17 13:59:51 -0300

    Add improviments in expense model
    - Add user relationship
    - Add file field
    - Add date field
    - Add currency field
    - Add choices from choices.py
    - Change __unicode__ to __str__

--- finances/models/expense.py
+++ finances/models/expense.py
@@ -1,29 +1,53 @@
 from django.db import models
 
-METHOD_TYPE = (
-		(1, 'Cash'),
-		(2, 'Credit Card'),
-		(3, 'Check')
-	)
+from django.contrib.auth.models import User
 
+from django.utils.translation import gettext as _
+from datetime import date as dat
+
+from .choices import (
+	CURRENCY_CHOICES,
+	METHOD_TYPE_CHOICES
+)
 
 class Expense(models.Model):
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE
+	)
 	type = models.ForeignKey(
-			'TypeExpense',
-			on_delete=models.CASCADE
-		)
+		'TypeExpense',
+		on_delete=models.CASCADE
+	)
 	value = models.PositiveIntegerField()
+	file = models.FileField(
+		upload_to='income/', 
+		blank=True
+	)
+	date = models.DateField(
+		default=dat.today
+	)
+	currency = models.CharField(
+		max_length=3, 
+		default='USD',
+		choices=CURRENCY_CHOICES
+	)
 	method = models.PositiveSmallIntegerField(
-			choices=METHOD_TYPE
-		)
+		choices=METHOD_TYPE_CHOICES
+	)
 	credit_card = models.ForeignKey(
-			'CreditCard',
-			on_delete=models.CASCADE
-		)
+		'CreditCard',
+		on_delete=models.CASCADE,
+		blank=True,
+		null=True
+	)
+
 
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
-	def __unicode__(self):
-		values = (self.type.name, self.value)
-		return '{} - {}'.format(values)
+	def __str__(self):
+		return '{} - {}'.format(
+				self.type.name,
+				self.value
+			)
