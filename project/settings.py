7e1c1e5fd55ee38785cac40be614ead09b8c73f2
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-03-17 18:20:53 -0300

    Add static configs

--- project/settings.py
+++ project/settings.py
@@ -126,7 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 try:
     from .local_settings import *
