ff214efe0bf6575b45db3bcaa540cfb31b254696
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-03-20 10:16:19 -0300

    Add override queryset in type fields and converter tabs to spaces

--- finance/admin/expense.py
+++ finance/admin/expense.py
@@ -1,21 +1,30 @@
 
 from django.contrib import admin
 from finance.models import Expense
-
+from finance.models import TypeExpense
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-	exclude = ('user',)
-	list_display = ('type','value','method','date',)
+    exclude = ('user',)
+    list_display = ('type','value','method','date',)
 
-	def get_queryset(self, request):
-		qs = super().get_queryset(request)
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        
+        if db_field.name == 'type':
+            # import pdb; pdb.set_trace()
+            kwargs['queryset'] = TypeExpense.objects.filter(user=request.user)
 
-		if not request.user.is_superuser:
-			return qs.filter(user=request.user).order_by('-date')
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+            
 
-		return qs
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
 
-	def save_model(self, request, obj, form, change):
-		obj.user = request.user
-		super().save_model(request, obj, form, change)
+        if not request.user.is_superuser:
+            return qs.filter(user=request.user).order_by('-date')
+
+        return qs
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
