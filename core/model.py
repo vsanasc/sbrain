3b3ed2dc4ae212847143e2dc508a3d6a70598949
Vitor Santos <vitor@funxperience.com>
2018-05-18 21:36:00 -0400

    Add core app

---
+++ core/model.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from .choices import STATUS_CHOICES
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    status = models.SmallIntegerField(choices=STATUS_CHOICES)
+
+    class Meta:
+        abstract=True
--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'core',
     'language.apps.LanguageConfig',
     'finance.apps.FinanceConfig',
 ]
