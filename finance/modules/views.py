16920b67d432df9597d96786183e90c32d55663c
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2018-06-01 06:56:55 -0400

    Fix WrapperView in modules views

--- finance/modules/views.py
+++ finance/modules/views.py
@@ -1,12 +1,21 @@
 from django.http import JsonResponse
+from django.views.generic import View
 
-# Create your views here.
 
-class ViewWrapper(View):
+class WrapperView(View):
 
     view_factory = None
 
     def get(self, request, *args, **kwargs):
+        kwargs.update(request.GET)
+        body, status = self.view_factory.create().get(**kwargs)
+
+        return JsonResponse(body, status=status)
+
+
+    def post(self, request, *args, **kwargs):
+        kwargs.update(request.GET)
+        kwargs.update(request.POST)
         body, status = self.view_factory.create().get(**kwargs)
 
         return JsonResponse(body, status=status)
