e42c2485d9531cad4a45fd7f5ef49eec6115ad8b
Vitor Santos <vitor@funxperience.com>
2018-06-10 18:24:17 -0400

    Rename model.py to models.py in core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from .choices import STATUS_CHOICES
@@ -10,3 +11,14 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+class Tag(BaseModel):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=30)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
