e1eeb91160f7f828acc9ae9525dee3980d6f9c45
Vitor S. do Nascimento <vitor@funxperience.com>
2018-10-13 17:55:03 -0300

    Add proficiency model functions

--- diary/models/date.py
+++ diary/models/date.py
@@ -20,5 +20,36 @@ class Date(BaseModel):
         ]
     )
 
+    def total_notes(self):
+        return self.smallnote_set.count()
+
+    def total_cycles(self):
+        cycles = 0
+        for i in self.dedication_set.all():
+            cycles += i.cycle
+
+        return cycles
+
+    def total_dedication(self):
+        minutes = 0
+        for i in self.dedication_set.all():
+            minutes += i.cycle * i.time
+
+        return minutes
+
+    def proficiency_resume(self):
+        cycles = self.total_cycles()
+        minutes = self.total_dedication()
+        time = round(float(minutes / 60), 1)
+        return "cycles: {} | time: {}H".format(cycles, time)
+
+    def proficiency_status(self):
+        if self.total_notes() > 2 and self.total_dedication() >= 120:
+            return '✅'
+        else:
+            return '❌'
+
+
+
     def __str__(self):
         return self.date.strftime('%Y-%m-%d')
